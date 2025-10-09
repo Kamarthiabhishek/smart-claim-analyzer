718f4683632bd90e4358d3d7c2e42b9d1ccd708e
Abhi Kamarthi <kamarthiabhishek@gmail.com>
2025-10-09 13:34:38 +0530

    Initial commit - Project setup

---
+++ src/main/java/com/insurance/smart_claim_analyzer/SmartInsuranceClaimAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.insurance.smart_claim_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartInsuranceClaimAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartInsuranceClaimAnalyzerApplication.class, args);
+	}
+
+}
